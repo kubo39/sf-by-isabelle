773fd0922b0453e1129515572d233e0358597d9d
Hiroki Noda <kubo39@gmail.com>
2019-02-22 00:51:07 +0900

    論理の基礎: 数値型

--- 1st/Basics.thy
+++ 1st/Basics.thy
@@ -9,36 +9,86 @@ datatype day = monday | tuesday | wednesday |
 
 fun next_weekday :: "day \<Rightarrow> day" where
   "next_weekday monday = tuesday"
-  | "next_weekday tuesday = wednesday"
-  | "next_weekday wednesday = thursday"
-  | "next_weekday thursday = friday"
-  | "next_weekday friday = monday"
-  | "next_weekday saturday = monday"
-  | "next_weekday sunday = monday"
+| "next_weekday tuesday = wednesday"
+| "next_weekday wednesday = thursday"
+| "next_weekday thursday = friday"
+| "next_weekday friday = monday"
+| "next_weekday saturday = monday"
+| "next_weekday sunday = monday"
 
 value "next_weekday friday" (* "monday" :: "day" *)
 value "next_weekday (next_weekday saturday)" (* "tuesday" :: "day" *)
 
 lemma test_next_weekday: "next_weekday (next_weekday saturday) = tuesday" by simp
 
-datatype bool = true | false
+datatype bool = True | False
 
 fun negb :: "bool \<Rightarrow> bool" where
-  "negb true = false"
-  | "negb false = true"
+  "negb True = False"
+| "negb False = True"
 
 fun andb :: "bool \<Rightarrow> bool \<Rightarrow> bool" where
-  "andb true b2 = b2"
-  | "andb false _ = false"
+  "andb True b2 = b2"
+| "andb False _ = False"
 
 fun orb :: "bool \<Rightarrow> bool \<Rightarrow> bool" where
-  "orb true _ = true"
-  | "orb false b2 = b2"
+  "orb True _ = True"
+| "orb False b2 = b2"
 
-lemma test_orb1: "orb true false = true" by simp
-lemma test_orb2: "orb false false = false" by simp
-lemma test_orb3: "orb false true = true" by simp
-lemma test_orb4: "orb true true = true" by simp
+lemma test_orb1: "orb True False = True" by simp
+lemma test_orb2: "orb False False = False" by simp
+lemma test_orb3: "orb False True = True" by simp
+lemma test_orb4: "orb True True = True" by simp
+
+
+fun pred :: "nat \<Rightarrow> nat" where
+  "pred 0 = 0"
+| "pred (Suc n') = n'"
+
+value "Suc (Suc (Suc (Suc 0)))" (* "4" :: "nat" *)
+
+fun minustwo :: "nat \<Rightarrow> nat" where
+  "minustwo 0 = 0"
+| "minustwo (Suc 0) = 0"
+| "minustwo (Suc (Suc n')) = n'"
+
+value "minustwo 4" (* "2" :: "nat" *)
+
+fun evenb :: "nat \<Rightarrow> bool" where
+  "evenb 0 = True"
+| "evenb (Suc 0) = False"
+| "evenb (Suc (Suc n')) = evenb n'"
+
+fun oddb :: "nat \<Rightarrow> bool" where
+  "oddb n = negb (evenb n)"
+
+lemma test_oddb1: "oddb 1 = True" by simp
+lemma test_oddb2: "oddb (Suc (Suc (Suc 1))) = False" by simp
+
+fun plus :: "nat \<Rightarrow> nat \<Rightarrow> nat" where
+  "plus 0 m = m"
+| "plus (Suc n') m = Suc (plus n' m)"
+
+value "plus 3 2" (* "5" :: "nat" *)
+
+fun mult :: "nat \<Rightarrow> nat \<Rightarrow> nat" where
+  "mult 0 _ = 0"
+| "mult (Suc n') m = plus m (mult n' m)"
+
+lemma test_plus1: "plus 1 1 = 2" by simp
+lemma test_plus2: "plus (Suc (Suc 1)) 3 = 6" by simp
+lemma test_mult1: "mult (Suc (Suc 1)) (Suc (Suc 1)) = 9" by simp
+
+fun minus :: "nat \<Rightarrow> nat \<Rightarrow> nat" where
+  "minus 0 _ = 0"
+| "minus n 0 = n"
+| "minus (Suc n') (Suc m') = minus n' m'"
+
+(*
+fun exp :: "nat \<Rightarrow> nat" where
+  "exp power 0 = Suc 0"
+| "exp base (Suc p) = mult base (exp base p)"
+*)
 
 end
 
