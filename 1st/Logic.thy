f223c79567f05dbdc5e3a53808433822bfeaf35c
Hiroki Noda <kubo39@gmail.com>
2019-02-28 07:33:35 +0900

    Logic: 論理積

--- 1st/Logic.thy
+++ 1st/Logic.thy
@@ -10,6 +10,8 @@ subsection {* Logical Connectives *}
 
 value "(mult 2 2)"
 
+subsubsection {* Conjunction *}
+
 lemma and_example: "3 + 4 = 7 \<and> (2::nat) * 2 = 4"
   apply (simp)
   done
@@ -17,6 +19,32 @@ lemma and_example: "3 + 4 = 7 \<and> (2::nat) * 2 = 4"
 lemma and_intro: "A \<longrightarrow> B \<longrightarrow> A \<and> B"
   apply (simp)
   done
+
+lemma and_exercise: "\<forall> n m::nat. n + m = 0 \<longrightarrow> n = 0 \<and> m = 0"
+  apply (simp)
+  done
+
+lemma and_example2: "\<forall> n m::nat. n = 0 \<and> m = 0 \<longrightarrow> n + m = 0"
+  apply (simp)
+  done
+
+lemma and_example3: "\<forall> n m::nat. n + m = 0 \<longrightarrow> n * m = 0"
+  apply (simp)
+  done
+
+lemma proj1: "P \<and> Q \<longrightarrow> P"
+  apply (simp)
+  done
+
+lemma proj2: "P \<and> Q \<longrightarrow> Q"
+  apply (simp)
+  done
+
+theorem and_commut: "P \<and> Q \<longrightarrow> Q \<and> P"
+  apply (simp)
+  done
+
+subsubsection {* Disjunction *}
 
 lemma or_example: "\<forall> n m :: nat. n = 0 \<or> m = 0 \<longrightarrow> n * m = 0"
   apply (simp)
