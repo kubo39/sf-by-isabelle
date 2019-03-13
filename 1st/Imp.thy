b15e680454e1fac5d977272d68dd7d333923a2a0
Hiroki Noda <kubo39@gmail.com>
2019-03-13 20:41:42 +0900

    Imp: 最適化: 0+e --> eにする

--- 1st/Imp.thy
+++ 1st/Imp.thy
@@ -34,4 +34,27 @@ fun beval :: "bexp \<Rightarrow> bool" where
 | "beval (BNot b1) = negb (beval b1)"
 | "beval (BAnd b1 b2) = andb (beval b1) (beval b2)"
 
+section {* Optimization *}
+
+fun optimize_0plus :: "aexp \<Rightarrow> aexp" where
+  "optimize_0plus (ANum n) = ANum n"
+| "optimize_0plus (APlus (ANum 0) e2) = e2"
+| "optimize_0plus (APlus e1 e2) = APlus (optimize_0plus e1) (optimize_0plus e2)"
+| "optimize_0plus (AMinus e1 e2) = AMinus (optimize_0plus e1) (optimize_0plus e2)"
+| "optimize_0plus (AMult e1 e2) = AMult (optimize_0plus e1) (optimize_0plus e2)"
+
+lemma test_optimize_0plus:
+  "(beval (BEq (optimize_0plus (APlus (ANum (Suc 1)) (APlus (ANum 0) (APlus (ANum 0) (ANum 1)))))
+           (APlus (ANum (Suc 1)) (ANum 1)))) = True"
+  apply (simp)
+  done
+
+theorem optimize_0plus_sound: "aeval (optimize_0plus a) = aeval a"
+  apply (induct a)
+     apply (simp) (* ANum *)
+    apply (case_tac a) (* APlus *)
+       apply (case_tac a1) (* a1 = ANum n *)
+          apply (simp)
+  oops
+
 end
